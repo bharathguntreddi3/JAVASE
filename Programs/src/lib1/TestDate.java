b73512c30d695019f7ffb819ee8e3a137d7123e3
bharathguntreddi3 <guntreddibharath@gmail.com>
2024-05-22 18:55:51 +0530

    datetime

---
+++ Programs/src/lib1/TestDate.java
@@ -0,0 +1,15 @@
+package lib1;
+
+import java.time.LocalDateTime;
+
+public class TestDate {
+
+	public static void main(String[] args) {
+
+		var dt = LocalDateTime.now();
+		
+		System.out.println(dt);
+		
+	}
+
+}

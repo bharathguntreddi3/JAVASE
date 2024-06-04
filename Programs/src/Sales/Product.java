71bc61ba2865468ac41c5f2ae9b586958088fa42
bharathguntreddi3 <guntreddibharath@gmail.com>
2024-06-04 11:30:20 +0530

    packages, JAR & RAR

---
+++ Programs/src/Sales/Product.java
@@ -0,0 +1,9 @@
+package Sales;
+
+class Supplier  {
+	// package level access
+}
+
+public class Product {
+    Supplier s;
+}
---
+++ Programs/src/Test/TestSales.java
@@ -0,0 +1,18 @@
+package Test;
+
+import Sales.Customer;
+import Sales.*;
+
+
+public class TestSales {
+
+	public static void main(String[] args) {
+		 Customer c;
+		 Sales.Product p;   // fully qualified classname 
+		 
+		 //sales.Supplier s; 
+		 
+
+	}
+
+}

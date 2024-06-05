dc6fc7ae229b7679fcbd86ee08da9f10c6e5c60f
bharathguntreddi3 <guntreddibharath@gmail.com>
2024-06-05 13:43:12 +0530

    ignored

--- upload.bat
+++ upload.bat
@@ -1,12 +1,8 @@
-checking the status of the files
 git status
 
-adding all files to rem
 git add .
 
-committing all files to the rem
 git commit -m %1
 
-push all files to the rem with master branch
 git push origin master
 

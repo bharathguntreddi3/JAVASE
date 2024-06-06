55e4134c72bf673f5f080d941fdb271c9ec825a7
bharathguntreddi3 <guntreddibharath@gmail.com>
2024-06-06 18:37:41 +0530

    TreeMap & Map Interface

--- Programs/src/collectionframework/GenericDemo.java
+++ Programs/src/collectionframework/GenericDemo.java
@@ -13,9 +13,13 @@ public class GenericDemo {
 		names.add(10);	// Integer
 		names.add("pqr");
 		
+		int n = (Integer) names .get(0);
+		
 		var coll = new ArrayList<String>();	// this says you can only have strings 
 //		coll.add(20);// shows error it doesn't add integer only string 
 		coll.add("First");
+		
+		String s = coll.get(0);
 	}
 
 }
